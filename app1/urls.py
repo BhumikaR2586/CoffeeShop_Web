03f8a0ce0888a4d0740d401ba873afe957ff5009
BhumikaR2586 <102bhumika11scia@gmail.com>
2025-04-07 11:08:22 +0530

    Added cart functionality

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
 from . import views
 
-urlpatterns = [
-    path('', views.home)
+urlpatterns =[
+    path('', views.home, name='home'),
+    path('add-to-cart/<int:coffee_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/increment/<int:cart_id>/', views.increment_quantity, name='increment_quantity'),
+    path('cart/decrement/<int:cart_id>/', views.decrement_quantity, name='decrement_quantity'),
 ]
--- projectmini/urls.py
+++ projectmini/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
